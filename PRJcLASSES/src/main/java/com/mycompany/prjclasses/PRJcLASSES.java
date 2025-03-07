02a662168c3f7500cda96a90b79441505f10aa6c
ST10471350 <scholtzkieran79@gmail.com>
2025-03-07 10:29:37 +0200

    First commit
    Added netBeans file

---
+++ PRJcLASSES/src/main/java/com/mycompany/prjclasses/PRJcLASSES.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prjclasses;
+
+/**
+ *
+ * @author schol
+ */
+public class PRJcLASSES {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World ! I did NOT do github and hate ARC now :(");
+    }
+}
