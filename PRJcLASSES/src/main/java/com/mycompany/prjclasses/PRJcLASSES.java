2058828a46fb40e8a922b33aa6f1bdfe228386f2
ST10471350 <scholtzkieran79@gmail.com>
2025-03-11 10:52:00 +0200

    Changes made

--- PRJcLASSES/src/main/java/com/mycompany/prjclasses/PRJcLASSES.java
+++ PRJcLASSES/src/main/java/com/mycompany/prjclasses/PRJcLASSES.java
@@ -11,6 +11,10 @@ package com.mycompany.prjclasses;
 public class PRJcLASSES {
 
     public static void main(String[] args) {
-        System.out.println("Hello World ! I did NOT do github and hate ARC now :(");
+        
+        newClass nc = new newClass();
+        nc.helloClass();
+        
+        System.out.println(nc.strSecondClassVar);
     }
 }
